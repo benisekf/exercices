55dad106e6370ae2b84d3d8d50daf7d2dd7020cb
benisekf <filip.benishek@gmail.com>
2024-01-16 19:31:30 +0100

    Add string_checker.py

---
+++ string_checker.py
@@ -0,0 +1,34 @@
+def is_valid_string(s):
+    stack = []
+
+    for char in s:
+        if char in '([{':
+            stack.append(char)
+        elif char in ')]}':
+            if not stack or (char == ')' and stack[-1] != '(') or (char == ']' and stack[-1] != '['):
+                return False
+            stack.pop()
+
+    return not stack
+
+print(is_valid_string("()"))      
+print(is_valid_string("([])"))     
+print(is_valid_string("{[()]}"))   
+print(is_valid_string("abc"))    
+print(is_valid_string("("))         
+print(is_valid_string(")"))       
+print(is_valid_string("(]"))        
+print(is_valid_string("{[)}"))      
+
+
+
+
+
+
+
+
+
+
+
+
+
